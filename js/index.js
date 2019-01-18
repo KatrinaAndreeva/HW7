6e8b13dfbfc030bcd7458c2a1a76867f6a7f95e9
katrin <andreevak53@gmail.com>
2019-01-18 23:32:04 +0000

    saved

--- js/index.js
+++ js/index.js
@@ -5,10 +5,14 @@ console.log(par.textContent);
 // 2. Создать функцию, которая принимает в качестве аргумента узел DOM и возвращает
 // информацию (в виде объекта) о типе узла, об имени узла и о количестве дочерних узлов (если детей нет - 0)ю
 // let nodeElement = document.querySelector('');
-function getInfoNode(node) {
-    return document.nodeType;
+  let obj=[];
+    let elem=document.querySelectorAll('p');
+    for (var i=0;i<elem.length;i++){
+        obj=[{type:elem[i].nodeType,name:elem[i].tagName,child_length:elem[i].children.length,child:elem[i]}];
+    }
+    console.log(obj);
 
-}
+
 // 3. Получить массив, который состоит из текстового содержимого ссылок внутри списка: getTextFromUl(ul) ---> ["Link1", "Link2", "Link3"]    ++++/-----
 
 let elements = document.querySelectorAll('li');
